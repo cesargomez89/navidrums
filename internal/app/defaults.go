c866687bc4cc721b06e4344700cbc9445fefe147
Cesar Gomez <cesargomez89@gmail.com>
2026-03-25 00:19:08 -0600

    feat: add GetMoods and GetStyles helper functions

--- internal/app/defaults.go
+++ internal/app/defaults.go
@@ -1,5 +1,10 @@
 package app
 
+import (
+	"encoding/json"
+	"github.com/cesargomez89/navidrums/internal/store"
+)
+
 var DefaultMoods = []string{
 	"Aggressive",
 	"Atmospheric",
@@ -25,4 +30,34 @@ var DefaultStyles = []string{
 	"Traditional",
 	"Urban",
 	"Crossover",
+}
+
+func GetMoods(settingsRepo *store.SettingsRepo) []string {
+	custom, err := settingsRepo.Get(store.SettingMoodList)
+	if err != nil || custom == "" {
+		return DefaultMoods
+	}
+	var list []string
+	if err := json.Unmarshal([]byte(custom), &list); err != nil {
+		return DefaultMoods
+	}
+	if len(list) == 0 {
+		return DefaultMoods
+	}
+	return list
+}
+
+func GetStyles(settingsRepo *store.SettingsRepo) []string {
+	custom, err := settingsRepo.Get(store.SettingStyleList)
+	if err != nil || custom == "" {
+		return DefaultStyles
+	}
+	var list []string
+	if err := json.Unmarshal([]byte(custom), &list); err != nil {
+		return DefaultStyles
+	}
+	if len(list) == 0 {
+		return DefaultStyles
+	}
+	return list
 }
