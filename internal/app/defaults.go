5c6297efa312912d56bd56f45110d6727dbb8b11
Cesar Gomez <cesargomez89@gmail.com>
2026-03-25 00:06:20 -0600

    feat: add default mood and style lists

---
+++ internal/app/defaults.go
@@ -0,0 +1,28 @@
+package app
+
+var DefaultMoods = []string{
+	"Aggressive",
+	"Atmospheric",
+	"Chill",
+	"Dark",
+	"Energetic",
+	"Melancholic",
+	"Mystical",
+	"Romantic",
+	"Sophisticated",
+	"Uplifting",
+}
+
+var DefaultStyles = []string{
+	"Acoustic",
+	"Alternative",
+	"Cinematic",
+	"Electronic",
+	"Hardcore",
+	"Lyricist",
+	"Pop",
+	"Rock",
+	"Traditional",
+	"Urban",
+	"Crossover",
+}
